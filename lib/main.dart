fd3cf67c12f5e40e8b85b23298e308ac75749fb0
techsharif <sharif.cse.hstu@gmail.com>
2020-02-25 16:36:37 +0600

    state add, make statelees to statefull

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-void buttonClick(){
-  print("button clicked");
+
+
+class MyApp extends StatefulWidget{
+
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget{
+class _MyAppState extends State<MyApp> {
+  int dataIndex = 0;
+  List<String> questions = [
+    "one",
+    "two"
+  ];
+  void buttonClick(){
+    setState(() {
+      dataIndex += 1;
+    });
+
+  }
+
 
   @override
   Widget build(BuildContext context){
     return MaterialApp(home: Scaffold(
       appBar: AppBar(
-        title: Text('one'),
+        title: Text("title"),
       ),
       body: Column(
         children: [
-          Text("One text"),
+          Text(questions.elementAt(dataIndex)),
           RaisedButton(
             child: Text("Button"),
             onPressed: buttonClick,
