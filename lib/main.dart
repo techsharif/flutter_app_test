f882a28b2b447c44a788df8185865cc83843238e
techsharif <sharif.cse.hstu@gmail.com>
2020-02-25 16:11:28 +0600

    add toast on button click

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
+
+  @override
   Widget build(BuildContext context){
     return MaterialApp(home: Scaffold(
       appBar: AppBar(
@@ -11,12 +13,28 @@ class MyApp extends StatelessWidget{
       body: Column(
         children: [
           Text("One text"),
-          RaisedButton(
-            child: Text("Button"),
-            onPressed: null,
-          ),
+
+        new Builder(
+            builder: (BuildContext context){
+              return new RaisedButton(
+                child: Text("Button"),
+                onPressed: () => _showToast(context),
+              );
+            }
+        ),
         ],
       ),
     ),);
+  }
+
+  void _showToast(BuildContext context) {
+    final scaffold = Scaffold.of(context);
+    scaffold.showSnackBar(
+      SnackBar(
+        content: const Text('WooW!!!!!'),
+        action: SnackBarAction(
+            label: 'UNDO', onPressed: scaffold.hideCurrentSnackBar),
+      ),
+    );
   }
 }
